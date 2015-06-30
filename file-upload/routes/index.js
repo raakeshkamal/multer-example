9e079433c64ef1d682b59f6b59927a2d936156e2
raakeshkamal <raakeshkamal@gmail.com>
2015-07-01 01:00:33 +0530

    first commit

---
+++ file-upload/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res) {
+  console.dir(req.files);
+});
+module.exports = router;
