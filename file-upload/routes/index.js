33273e29c6e647b74f2d164f4344993548804516
Larry Maddocks <larrym14@gmail.com>
2015-07-02 11:19:15 -0600

    Moved multer code to here.
    Server was not sending a response back after uploading. Moved multer code to router code and updated the post response.  This seems to be working now.

--- file-upload/routes/index.js
+++ file-upload/routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var multer = require("multer");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/',[ multer({ dest: './uploads/'}), function(req, res){
+    console.log(req.body); // form fields
+    console.log(req.files); // form files
+    res.status(204).end();
+}]);
 router.post('/', function(req, res) {
   console.dir(req.files);
 });
